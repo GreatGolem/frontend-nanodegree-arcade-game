40da1aaa0ce60b360e3a906b5f0d9420469f9ac7
GreatGolem-laptop <siyu.wu.91@gmail.com>
2016-08-23 10:33:11 -0400

    1st review
    Collision function moved. README added.

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 frontend-nanodegree-arcade-game
 ===============================
+## How to Run
+To run the game, simply open the **index.html** with your browser. If the html file is not bind to the browser you want to use, you can drag it into a browser window.
 
-Students should use this [rubric](https://review.udacity.com/#!/projects/2696458597/rubric) for self-checking their submission. Make sure the functions you write are **object-oriented** - either class functions (like Player and Enemy) or class prototype functions such as Enemy.prototype.checkCollisions, and that the keyword 'this' is used appropriately within your class and class prototype functions to refer to the object the function is called upon. Also be sure that the **readme.md** file is updated with your instructions on both how to 1. Run and 2. Play your arcade game.
-
-For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+## How to Play
+* Use arrow keys to control the character.
+* Avoid the bugs and reach the river to win.
--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 // Enemies our player must avoid
+var gridW = 101;
+var gridH = 83;
+var gridHOffset = 60;
+
 var Enemy = function(x,y,speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
@@ -18,100 +22,100 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += this.speed*dt;
-    if(this.x > 101*5) {
-        this.x = 101*(Math.random()*-3);
+    if(this.x > gridW*5) {
+        this.x = gridW*(Math.random()*-3);
     }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
     this.sprite = 'images/char-cat-girl.png';
-    this.x = 101*2;
-    this.y = 60 + 83*4;
-}
+    this.x = gridW*2;
+    this.y = gridHOffset + gridH*4;
+};
 
 //Update player position on map. Check collision with all enemies.
 //Reset player if collide with an enemy.
 Player.prototype.update = function(allEnemies) {
     var i, len = allEnemies.length;
     for (i=0; i<len; i++) {
-        if(collision(this,allEnemies[i])) {
+        if(this.collision(allEnemies[i])) {
             this.reset();
             break;
         }
     }
-}
+};
 
 //Draw player on map.
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 //Reset player to the bottom of the map.
 Player.prototype.reset = function() {
-    this.x = 101*2;
-    this.y = 60 + 83*4;
-}
+    this.x = gridW*2;
+    this.y = gridHOffset + gridH*4;
+};
 
 //Move character according to keyboard input. Do not move if at
 //boundary. Reset player if reaches the river.
 Player.prototype.handleInput = function(key) {
     if(key == 'left') {
         if(this.x > 0) {
-            this.x -= 101;
+            this.x -= gridW;
         }
     }
     if(key == 'right') {
-        if(this.x < 101*4) {
-            this.x += 101;
+        if(this.x < gridW*4) {
+            this.x += gridW;
         }
     }
   // If player reaches the river reset player to the bottom.
     if(key == 'up') {
-        if(this.y > 60) {
-            this.y -= 83;
-        } else if (this.y <= 60) {
+        if(this.y > gridHOffset) {
+            this.y -= gridH;
+        } else if (this.y <= gridHOffset) {
             this.reset();
         }
     }
     if(key == 'down') {
-        if(this.y < 60 + 83*4) {
-          this.y += 83;
+        if(this.y < gridHOffset + gridH*4) {
+          this.y += gridH;
         }
     }
-}
+};
 
 // A helper function to check collision. Input player and an emeny.
 // Output true or false.
-var collision = function(player, enemy) {
+Player.prototype.collision = function(enemy) {
     if(
-        player.y == enemy.y &&
-        player.x <= enemy.x + 81 &&
-        player.x >= enemy.x - 81
+        this.y == enemy.y &&
+        this.x <= enemy.x + 81 &&
+        this.x >= enemy.x - 81
     ) {
         return true;
     } else {
         return false;
     }
-}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-//Start each enemy at a random x coordinate on map. 
+//Start each enemy at a random x coordinate on map.
 var allEnemies = [
-  enemy1 = new Enemy(101 * (-1 + Math.random()*6), 60 + 83*2, 150),
-  enemy2 = new Enemy(101 * (-1 + Math.random()*6), 60 + 83, 200),
-  enemy3 = new Enemy(101 * (-1 + Math.random()*6), 60, 500),
-  enemy4 = new Enemy(101 * (-1 + Math.random()*6), 60 + 83*2, 150),
-  enemy5 = new Enemy(101 * (-1 + Math.random()*6), 60 + 83, 200)
+  enemy1 = new Enemy(gridW * (-1 + Math.random()*6), gridHOffset + gridH*2, 150),
+  enemy2 = new Enemy(gridW * (-1 + Math.random()*6), gridHOffset + gridH, 200),
+  enemy3 = new Enemy(gridW * (-1 + Math.random()*6), gridHOffset, 500),
+  enemy4 = new Enemy(gridW * (-1 + Math.random()*6), gridHOffset + gridH*2, 150),
+  enemy5 = new Enemy(gridW * (-1 + Math.random()*6), gridHOffset + gridH, 200)
 ]
 
 var player = new Player();
